92fec46c57ff5878301452affa3762fb4ced129b
Ksenia <yoshasoul@gmail.com>
2025-05-28 11:37:45 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InterprocessCommunication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
